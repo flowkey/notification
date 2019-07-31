3db79a25814ce92122d691eab7ce27b6ab099236
Marc Nitzsche <marc.nitzsche@live.de>
2019-07-31 15:51:39 +0200

    Add error handling for promises

--- enhanceWithPromiseSupport.js
+++ enhanceWithPromiseSupport.js
@@ -1,12 +1,20 @@
 // if a callback is provided then don't resolve but use provided callback
 export const enhanceWithPromiseSupport = notification => ({
     confirm: args =>
-        new Promise(resolve => {
-            notification.confirm({ callback: resolve, ...args });
+        new Promise((resolve, reject) => {
+            try {
+                notification.confirm({ callback: resolve, ...args });
+            } catch (e) {
+                reject(e);
+            }
         }),
     alert: args =>
-        new Promise(resolve => {
-            notification.alert({ callback: resolve, ...args });
+        new Promise((resolve, reject) => {
+            try {
+                notification.alert({ callback: resolve, ...args });
+            } catch (e) {
+                reject(e);
+            }
         }),
     close: args => notification.close(args),
 });
